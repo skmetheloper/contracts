540e332dd7606f092d25f041e629be41ae6b56d6
Etherio <50423290+etherio@users.noreply.github.com>
2020-07-07 16:25:27 +0630

    Update Container.php

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -2,36 +2,48 @@
 
 namespace Ethereal\Contracts\Container;
 
+use Psr\Container\ContainerInterface;
 use Ethereal\Contracts\Foundation\Application;
 
-interface Container
+interface Container implements ContainerInterface
 {
     /**
-     * 
-     * @param  string $name
+     * Check the entry of the container by its name.
+     *
+     * @param  string $name Identifer of the entry to look for.
+     *
      * @return bool
      */
     public function has($name);
 
     /**
-     * 
-     * @param  string $name
+     * Return the entry of the container by its name.
+     *
+     * @param  string $name Identifier of the entry to look for.
+     *
+     * @throws \Ethereal\Contracts\Container\NotFoundException
+     *
      * @return mixed
-     * @throws \OutOfBoundsException
      */
     public function get($name);
 
     /**
+     * Register new entry of the container by its name.
      * 
-     * @param  string           $accessor
-     * @param  string|\Closure  $concrete
+     * @param  string           $name     Identifer of the entry to register.
+     * @param  string|callable  $concrete The value of the entry to register.
+     * @param  bool             $force    Default is `false`. Overwrite it if the entry name is already existed.
+     *
+     * @throws \Ethereal\Contracts\Container\DuplicatedBindingException
+     *
      * @return void
      */
-    public function bind($accessor, $concrete);
+    public function bind($name, $concrete, $force = false);
 
     /**
      * 
      * @param  \Ethereal\Contracts\Container\Container $instance
+     *
      * @return void
      */
     public static function setInstance($instance);
